19feddf98b8c269151961e6f82a4f49491c25008
macbook <mo7amedaliebaid@gmail.com>
2025-02-23 12:22:03 +0200

    Initial Commit

---
+++ testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by Macbook on 23/02/2025.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
